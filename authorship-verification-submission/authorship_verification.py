86391dea295a43a11df1882fb8d99987304d46cb
Simon <simonruth@web.de>
2024-04-25 19:00:43 +0000

    removed similarity from approach, calculating TF/IDF directly on texts

--- authorship-verification-submission/authorship_verification.py
+++ authorship-verification-submission/authorship_verification.py
@@ -17,93 +17,37 @@ def write_answers(dataset_path: Path, predicted: list) -> None:
 
 tira = Client()
 
-    # loading train data
-text_train = tira.pd.inputs(
-        "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training")
-targets_train = tira.pd.truths(
-        "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training")
-    # loading validation data (automatically replaced by test data when run on tira)
-text_validation = tira.pd.inputs(
-        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training")
-targets_validation = tira.pd.truths(
-        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training")
+# Loading train data
+text_train = tira.pd.inputs("nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training")
+targets_train = tira.pd.truths("nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training")
 
-# ground_truth = pd.read_json("train/truth.jsonl", lines=True)
-# train_data = pd.read_json("train/pairs.jsonl", lines=True)
-# test_data = pd.read_json("test/pairs.jsonl", lines=True)
+# Loading validation data (automatically replaced by test data when run on tira)
+text_validation = tira.pd.inputs("nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training")
+targets_validation = tira.pd.truths("nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training")
 
-X = pd.DataFrame({"similarity":[],
-                         "id":[]})
+vectorizer = TfidfVectorizer()
 
-X_test_output = pd.DataFrame({"similarity":[],
-                         "id":[]})
-# Y = ground_truth["generated"] # uncomment to go quicker
-Y_train = []
-for index, text_snippets in enumerate(text_train["text"]):
-    print(text_snippets)
+# Fit the vectorizer on the training data and transform the text data into TF-IDF vectors
+X_train = vectorizer.fit_transform(text_train['text'])
 
-    truth_class = targets_train["generated"][index]
+# Transform the validation/test data into TF-IDF vectors
+X_validation = vectorizer.transform(text_validation['text'])
 
-    vectorizer1 = TfidfVectorizer()
+# Extract labels
+y_train = targets_train['generated']
+y_validation = targets_validation['generated']
 
-    tf_idf_features1 = vectorizer1.fit_transform(text_snippets)
-    # print(vectorizer1.get_feature_names_out())
+# Train model
+classifier = LogisticRegression() # Logistic Classifier with well interpretable results
+classifier.fit(X_train, y_train)
 
-    matrix = ((tf_idf_features1 * tf_idf_features1.T).A)
+# Predict on validation/test data
+y_pred = classifier.predict(X_validation)
 
-    dim = 1  # as only one doc in first category
-    # TODO ok to only do this over the 2 documents?
-    similarity = matrix[dim:, :dim].mean()
+# Calculate accuracy on validation/test data
+accuracy = accuracy_score(y_validation, y_pred)
+print("Accuracy:", accuracy)
 
-    df2 = pd.DataFrame({"similarity": [similarity],
-                        "id": [text_train["id"][index]]})
-
-    X = X.append(df2)
-    Y_train.append(truth_class)
-
-    print(matrix)
-    print(similarity)
-    print(truth_class)
-    if index == 20:
-        break
-
-for index, text_snippets in enumerate(text_validation["text"]):
-
-
-    vectorizer1 = TfidfVectorizer()
-
-    tf_idf_features1 = vectorizer1.fit_transform(text_snippets)
-    # print(vectorizer1.get_feature_names_out())
-
-    matrix = ((tf_idf_features1 * tf_idf_features1.T).A)
-
-    dim = 1  # as only one doc in first category
-    # TODO ok to only do this over the 2 documents?
-    similarity = matrix[dim:, :dim].mean()
-
-    df2 = pd.DataFrame({"similarity": [similarity], "id": [text_validation["id"][index]]})
-
-    X_test_output = X_test_output.append(df2)
-
-print(X)
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y_train, test_size=0.30) #TODO do we have to choose similarity from dataframe?
-
-
-Lr = LogisticRegression()
-X_train_reshape= np.array(X_train["similarity"]).reshape(-1, 1)
-X_test_reshape= np.array(X_test["similarity"]).reshape(-1, 1)
-Lr.fit(X_train_reshape, y_train)
-y_predict = Lr.predict(X_test_reshape)
-#df1 = pd.DataFrame({"Actual": y_test, "Predicted": y_predict})
-
-X_test_output_reshape= np.array(X_test_output["similarity"]).reshape(-1, 1)
-y_predict_output = Lr.predict(X_test_output_reshape)
-
-
-predicted = [{'id': x, 'generated': str(y)} for x, y in zip(X_test_output['id'], y_predict_output)]
-write_answers("test/", predicted)
-
-print(predicted)
-accuracy = accuracy_score(y_test, y_predict)
-print(accuracy)
+# Write predictions to file #TODO replace this by a better method
+predicted = [{'id': id_, 'generated': str(pred)} for id_, pred in zip(text_validation['id'], y_pred)]
+# write_answers("test/", predicted)
