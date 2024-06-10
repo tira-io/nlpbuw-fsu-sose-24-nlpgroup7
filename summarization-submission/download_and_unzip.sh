74c72213eae3723408521eea0fb1c9f0723dcbb0
Simon <63604385+Simrut@users.noreply.github.com>
2024-06-10 13:37:54 +0200

    updating model to bigger model zip

--- summarization-submission/download_and_unzip.sh
+++ summarization-submission/download_and_unzip.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Define the Google Drive file ID and the desired output filename
-FILE_ID="12px2bxEkuhaRXIXJUzubdd0gEjCkHiHh"
+FILE_ID="1v1wa0EbzbuIaxGRTh-i24CW8tI9xOSap"
 ZIP_FILE="trained_model.zip"
-
 
 # Download the zip file using gdown
 gdown --id $FILE_ID -O /code/$ZIP_FILE
